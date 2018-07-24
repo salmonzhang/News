86adc4e911613a3f9f7a784d7b9988be8941ca52
salmonzhang <ahzhangxuping@163.com>
2018-07-24 23:54:32 +0800

    viewpager原生动画DepthPageTransformer的详细分析

--- app/src/main/java/com/example/news/view/DepthPageTransformer.java
+++ app/src/main/java/com/example/news/view/DepthPageTransformer.java
@@ -10,38 +10,59 @@ import android.view.View;
  */
 
 public class DepthPageTransformer implements ViewPager.PageTransformer {
-    private static final float MIN_SCALE = 0.75f;
+    private static final float MIN_SCALE = 0.75f;//最小缩放
 
+    //viewpager滑动时，该方法实时执行
     public void transformPage(View view, float position) {
-        int pageWidth = view.getWidth();
 
+        System.out.println("view:" + view);
+        System.out.println("position" + position);
+
+        /**
+         *分析结论：
+         * view是viewpager的初始化方法中返回的view对象，view是多个不同的对象
+         * A页面的position,[0 , -1]
+         * B页面的position,[1 , 0]
+         */
+
+        int pageWidth = view.getWidth();
+        System.out.println("pageWidth" + pageWidth);
+        //pageWidth，为viewpager中单个view对象的宽度
+
+        //根据postion的取值范围，区分A,B页面并设置不同的动画
         if (position < -1) { // [-Infinity,-1)
             // This page is way off-screen to the left.
-            view.setAlpha(0);
-
+            view.setAlpha(0);//设置最左边的图片不可见
         } else if (position <= 0) { // [-1,0]
+            //A页面的position,[0 , -1]
             // Use the default slide transition when moving to the left page
-            view.setAlpha(1);
-            view.setTranslationX(0);
-            view.setScaleX(1);
-            view.setScaleY(1);
+            view.setAlpha(1);//设置A页面完全可见
+            view.setTranslationX(0);//设置A页面在X轴不移动，A页面的移动是viewpager自身的
+            view.setScaleX(1);//设置A页面在X轴不缩放
+            view.setScaleY(1);//设置A页面在Y轴不缩放
 
         } else if (position <= 1) { // (0,1]
+            //B页面的position,[1 , 0]
             // Fade the page out.
+            //透明度的取值范围：[0,1]
             view.setAlpha(1 - position);
 
             // Counteract the default slide transition
-            view.setTranslationX(pageWidth * -position);
+            //X轴的取值范围[-320,0]
+            view.setTranslationX(pageWidth * -position);//设置B页面不管怎么滑动，都始终保证显示在屏幕的正中间
 
             // Scale the page down (between MIN_SCALE and 1)
-            float scaleFactor = MIN_SCALE
+            //缩放值的范围[0.75 , 1]
+            //0.75 + 0.25 * 0 = 0.75
+            //0.75 + 1 * 0.25 = 1
+             float scaleFactor = MIN_SCALE
                     + (1 - MIN_SCALE) * (1 - Math.abs(position));
-            view.setScaleX(scaleFactor);
+            view.setScaleX(scaleFactor);//设置B页面的缩放
             view.setScaleY(scaleFactor);
 
         } else { // (1,+Infinity]
             // This page is way off-screen to the right.
-            view.setAlpha(0);
+            view.setAlpha(0);//设置最右边的图片不可见
         }
     }
 }
