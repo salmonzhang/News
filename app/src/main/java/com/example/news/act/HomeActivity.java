1444ff9d0ced0e0bd17fcae51da67f98dd4a43df
salmonzhang <ahzhangxuping@163.com>
2018-08-26 23:01:05 +0800

    使用SlidingMenu设置侧滑菜单

--- app/src/main/java/com/example/news/act/HomeActivity.java
+++ app/src/main/java/com/example/news/act/HomeActivity.java
@@ -1,15 +1,50 @@
 package com.example.news.act;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
 import com.example.news.R;
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends SlidingFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        //设置内容界面的视图
+        setContentView(R.layout.activity_home);//不是原来的setContentView
+
+        //设置第一个菜单界面的视图
+        setBehindContentView(R.layout.activity_menu);
+
+        //获取侧滑菜单对象
+        SlidingMenu slidingMenu = getSlidingMenu();
+
+        //设置显示模式
+        /**
+         * SlidingMenu.LEFT 菜单显示在左边
+         * SlidingMenu.RIGHT 菜单显示在右边
+         * SlidingMenu.LEFT_RIGHT 菜单显示在左右边
+         */
+        slidingMenu.setMode(SlidingMenu.LEFT);
+
+        //设置侧滑菜单的触摸模式
+        /**
+         * SlidingMenu.TOUCHMODE_FULLSCREEN 全屏模式
+         * SlidingMenu.TOUCHMODE_MARGIN 边缘模式
+         * SlidingMenu.TOUCHMODE_NONE 禁止模式
+         */
+        slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+
+        //设置菜单界面显示之后内容界面显示宽度
+        slidingMenu.setBehindOffsetRes(R.dimen.slidingmenu_width);//能够通过dimens.xml直接在代码写入dp值
+
+        //设置菜单与内容界面的阴影效果
+        slidingMenu.setShadowDrawable(R.drawable.shadow);
+        slidingMenu.setShadowWidthRes(R.dimen.shadow_width);
+
+        //如果要左右都显示必须设置第二个菜单
+//        slidingMenu.setSecondaryMenu(R.layout.activity_menu);
+//        slidingMenu.setSecondaryShadowDrawable(R.drawable.shadow);
     }
 }
