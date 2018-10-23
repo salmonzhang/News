2273e212a4aa6307b2d905292c8098eb8c7111ae
salmonzhang <ahzhangxuping@163.com>
2018-10-23 23:20:11 +0800

    添加轮播图

--- app/src/main/java/com/example/news/home/NewItemPage.java
+++ app/src/main/java/com/example/news/home/NewItemPage.java
@@ -1,11 +1,13 @@
 package com.example.news.home;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Handler;
 import android.os.Message;
 import android.view.View;
 import android.widget.ListView;
 
+import com.example.news.R;
 import com.example.news.adapter.NewItemAdapter;
 import com.example.news.bean.NewItemBean;
 import com.example.news.utils.GsonTools;
@@ -30,12 +32,19 @@ import okhttp3.Response;
 public class NewItemPage extends BasePage {
     private final String mUrl;
     private ListView mLv;
+    @SuppressLint("HandlerLeak")
     private Handler mHandler = new Handler(){
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
             switch (msg.what) {
                 case 0:
+//                    RollViewPager rollViewPager = new RollViewPager(mContext);
+                    int headerViewsCount = mLv.getHeaderViewsCount();//获取头视图的个数
+                    if (headerViewsCount == 0) {//防止头视图重复加载
+                        mLv.addHeaderView(mTopView);
+                    }
+
                     if (mNewItemAdapter == null) {
                         mNewItemAdapter = new NewItemAdapter(mContext, newItems);
                         mLv.setAdapter(mNewItemAdapter);
@@ -47,6 +56,7 @@ public class NewItemPage extends BasePage {
         }
     };
     private NewItemAdapter mNewItemAdapter;
+    private View mTopView;
 
     public NewItemPage(Context context, String url) {
         super(context);
@@ -55,7 +65,11 @@ public class NewItemPage extends BasePage {
 
     @Override
     public View initView() {
-        mLv = new ListView(mContext);
+        View view = View.inflate(mContext, R.layout.frag_item_news, null);
+        mLv = view.findViewById(R.id.lv_item_news);
+
+        //初始化头视图的view
+        mTopView = View.inflate(mContext, R.layout.layout_roll_view, null);
         return mLv;
     }
 
@@ -76,6 +90,7 @@ public class NewItemPage extends BasePage {
 
             @Override
             public void onResponse(Call call, Response response) throws IOException {
+                isLoad = true;//防止头视图重复加载
                 String json = response.body().string();
                 parseJson(json);
             }
---
+++ app/src/main/java/com/example/news/view/RollViewPager.java
@@ -0,0 +1,23 @@
+package com.example.news.view;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+
+/**
+ * author:salmonzhang
+ * Description:专门实现轮播图的功能
+ * Date:2018/10/23 0023 22:57
+ */
+
+public class RollViewPager extends ViewPager{
+
+    public RollViewPager(Context context) {
+        super(context);
+    }
+
+    public RollViewPager(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
